06e37c724a6397aac23ac28fab6d44ef67acd23d
Mark Taratynov <mark.taratynov@gmail.com>
2023-11-21 22:16:05 +0100

    added comments

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
-// Weather API key and base URL
+// Weather API key for authentication
 const apiKey = '7745ad0a99b44eb8a1b165946232111';
+
+// Base URL for the Weather API
 const baseUrl = 'http://api.weatherapi.com/v1';
 
-// City for weather information
+// City for which weather information is requested
 const city = 'Stockholm';
 
 // Construct the full API URL with parameters
@@ -11,23 +13,37 @@ const apiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${city}`;
 // Function to fetch data using XMLHttpRequest and return a Promise
 function fetchData(url) {
     return new Promise((resolve, reject) => {
+        // Create a new XMLHttpRequest object
         const xhr = new XMLHttpRequest();
+
+        // Configure the request: GET request to the specified URL
         xhr.open('GET', url, true);
 
+        // Set the event handler function for when data is loaded successfully
         xhr.onload = function () {
+            // Check if the request was successful (status code 200)
             if (xhr.status === 200) {
+                // Parse the JSON response
                 const data = JSON.parse(xhr.responseText);
+                
+                // Resolve the Promise with the parsed data
                 resolve(data);
+
+                // Log the data to the console
                 console.log(data);
             } else {
+                // If the request was not successful, reject the Promise with the status text
                 reject(xhr.statusText);
             }
         };
 
+        // Set the event handler function for network errors
         xhr.onerror = function () {
+            // Reject the Promise with a network error message
             reject('Network error occurred');
         };
 
+        // Send the request
         xhr.send();
     });
 }
