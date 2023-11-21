7fc6c07cfa0017f4c6be6a6dc4a6d330fbeeb82a
Mark Taratynov <mark.taratynov@gmail.com>
2023-11-21 22:15:06 +0100

    fixes

--- api.js
+++ api.js
@@ -1,15 +1,14 @@
-// Weather API key for authentication
+// Weather API key and base URL
 const apiKey = '7745ad0a99b44eb8a1b165946232111';
-
-// Base URL for the Weather API
 const baseUrl = 'http://api.weatherapi.com/v1';
 
-// City for which weather information is requested
+// City for weather information
 const city = 'Stockholm';
 
 // Construct the full API URL with parameters
 const apiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${city}`;
 
+// Function to fetch data using XMLHttpRequest and return a Promise
 function fetchData(url) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
@@ -33,16 +32,20 @@ function fetchData(url) {
     });
 }
 
-// Use the fetchData function
+// Use the fetchData function to get weather data
 fetchData(apiUrl)
     .then((data) => {
+        // Extract location and temperature from the data
         const location = data.location.name;
         const temperature = data.current.temp_c;
+
+        // Update HTML elements with weather information
         const locationElement = document.getElementById('location');
         const temperatureElement = document.getElementById('temperature');
         temperatureElement.textContent = `Current Temperature: ${temperature}°C`;
         locationElement.textContent = `Location: ${location}`;
     })
     .catch((error) => {
+        // Handle errors
         console.error('Error: ', error);
     });
