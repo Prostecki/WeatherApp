d4353f5f5130e75d3d2892924fb4e383dcc3222d
Mark Taratynov <mark.taratynov@gmail.com>
2023-12-03 22:33:31 +0100

    structurized js#2

--- js/dom.js
+++ js/dom.js
@@ -1,38 +1,35 @@
-
-// nextButton.addEventListener('click', )
-
-// export function welcomeWindow() {
+export function welcomeWindow() {
     
-//     clearPage();
+    clearPage();
     
-//     const nextButton = document.createElement('button');
-//     nextButton.classList.add('welcome-nextbutton');
+    const nextButton = document.createElement('button');
+    nextButton.classList.add('welcome-nextbutton');
 
-//     const welcomeModal = document.createElement('div');
-//     welcomeModal.classList.add('welcome-modal');
+    const welcomeModal = document.createElement('div');
+    welcomeModal.classList.add('welcome-modal');
 
-//     const headingBox = document.createElement('div');
-//     headingBox.classList.add('heading-box');
+    const headingBox = document.createElement('div');
+    headingBox.classList.add('heading-box');
 
-//     const welcomeHeading = document.createElement('h1');
-//     welcomeHeading.textContent = 'Weathyy';
-//     welcomeHeading.classList.add('welcomeModal-heading');
+    const welcomeHeading = document.createElement('h1');
+    welcomeHeading.textContent = 'Weathyy';
+    welcomeHeading.classList.add('welcomeModal-heading');
 
-//     const welcomeUnderHeading = document.createElement('p');
-//     welcomeUnderHeading.textContent = 'Weather App';
-//     welcomeUnderHeading.classList.add('welcome-underHeading');
+    const welcomeUnderHeading = document.createElement('p');
+    welcomeUnderHeading.textContent = 'Weather App';
+    welcomeUnderHeading.classList.add('welcome-underHeading');
 
-//     const welcomeImg = document.createElement('img');
-//     welcomeImg.src = 'img/umbrella_start.png';
-//     welcomeImg.classList.add('welcome-img');
+    const welcomeImg = document.createElement('img');
+    welcomeImg.src = 'img/umbrella_start.png';
+    welcomeImg.classList.add('welcome-img');
 
-//     //Append two elements into main div with class "headingBox";
-//     headingBox.appendChild(welcomeHeading);
-//     headingBox.appendChild(welcomeUnderHeading);
+    //Append two elements into main div with class "headingBox";
+    headingBox.appendChild(welcomeHeading);
+    headingBox.appendChild(welcomeUnderHeading);
 
-//     welcomeModal.appendChild(welcomeImg);
-//     welcomeModal.appendChild(headingBox);
-//     welcomeModal.appendChild(nextButton);
+    welcomeModal.appendChild(welcomeImg);
+    welcomeModal.appendChild(headingBox);
+    welcomeModal.appendChild(nextButton);
 
-//     body.appendChild(welcomeModal);
-// };
+    body.appendChild(welcomeModal);
+};
