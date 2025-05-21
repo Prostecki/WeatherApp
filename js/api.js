7bb33fed11cb9fb8762903a9a7fbc3d475aace73
Mark Taratynov <mark.taratynov@gmail.com>
2025-05-21 21:18:38 +0200

    Refactor weather app: streamline DOM manipulation, remove unused files, and enhance styling with CSS variables for light and dark themes

--- js/api.js
+++ js/api.js
@@ -1,116 +1,120 @@
 // Function to fetch data using XMLHttpRequest and return a Promise
 export function fetchData(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.onload = () => (xhr.status === 200) ? resolve(JSON.parse(xhr.responseText)) : reject(xhr.statusText);
-        xhr.onerror = () => reject('Network error occurred');
-        xhr.send();
-    });
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = () =>
+      xhr.status === 200
+        ? resolve(JSON.parse(xhr.responseText))
+        : reject(xhr.statusText);
+    xhr.onerror = () => reject("Network error occurred");
+    xhr.send();
+  });
 }
 
 // Function to display weather data
 export function displayWeather(data, astronomyData) {
-    const { current, location, astronomy } = data;
-    const elements = {
-        location: document.getElementById('location'),
-        localTime: document.getElementById('localTime'),
-        temperature: document.getElementById('temperature'),
-        conditionText: document.querySelector('.condition-text'),
-        conditionImg: document.querySelector('.condition-img'),
-        feelslike: document.querySelector('.feelslike-c'),
-        feelslikeF: document.querySelector('.feelslike-f'),
-        humidity: document.querySelector('.humidity'),
-        wind: document.querySelector('.wind'),
-        sunrise: document.querySelector('.sunrise'),
-        sunset: document.querySelector('.sunset'),
-        uv: document.querySelector('.uv'),
-        vixibility: document.querySelector('.visibility'),
-    };
+  const { current, location, astronomy } = data;
+  const elements = {
+    location: document.getElementById("location"),
+    localTime: document.getElementById("localTime"),
+    temperature: document.getElementById("temperature"),
+    conditionText: document.querySelector(".condition-text"),
+    conditionImg: document.querySelector(".condition-img"),
+    feelslike: document.querySelector(".feelslike-c"),
+    feelslikeF: document.querySelector(".feelslike-f"),
+    humidity: document.querySelector(".humidity"),
+    wind: document.querySelector(".wind"),
+    sunrise: document.querySelector(".sunrise"),
+    sunset: document.querySelector(".sunset"),
+    uv: document.querySelector(".uv"),
+    vixibility: document.querySelector(".visibility"),
+  };
 
-    elements.temperature.textContent = `${current.temp_c}°C`;
-    elements.location.textContent = `${location.name}, ${location.country}`;
-    elements.localTime.textContent = `${location.localtime}`;
-    elements.conditionText.textContent = `${current.condition.text}`;
-    elements.conditionImg.src = `${current.condition.icon}`;
-    elements.feelslike.textContent = `Feels like: ${current.feelslike_c}°C`;
-    elements.feelslikeF.textContent = `Feels like: ${current.feelslike_f}F`;
-    elements.humidity.textContent = `Humidity: ${current.humidity}%`;
-    elements.wind.textContent = `Wind: ${current.wind_kph} km/h`;
-    elements.uv.textContent = `UV: ${current.uv}`;
-    elements.vixibility.textContent = `Visibility: ${current.vis_km} km`;
+  elements.temperature.textContent = `${current.temp_c}°C`;
+  elements.location.textContent = `${location.name}, ${location.country}`;
+  elements.localTime.textContent = `${location.localtime}`;
+  elements.conditionText.textContent = `${current.condition.text}`;
+  elements.conditionImg.src = `${current.condition.icon}`;
+  elements.feelslike.textContent = `Feels like: ${current.feelslike_c}°C`;
+  elements.feelslikeF.textContent = `Feels like: ${current.feelslike_f}F`;
+  elements.humidity.textContent = `Humidity: ${current.humidity}%`;
+  elements.wind.textContent = `Wind: ${current.wind_kph} km/h`;
+  elements.uv.textContent = `UV: ${current.uv}`;
+  elements.vixibility.textContent = `Visibility: ${current.vis_km} km`;
 
+  // Display astronomy data
+  if (astronomyData && astronomyData.astro) {
+    const { sunrise, sunset } = astronomyData.astro;
 
-    // Display astronomy data
-    if (astronomyData && astronomyData.astro) {
-        const { sunrise, sunset } = astronomyData.astro;
-        
-        console.log('Astronomy Data:', astronomyData);
-        console.log('Sunrise:', sunrise);
-        console.log('Sunset:', sunset);
+    console.log("Astronomy Data:", astronomyData);
+    console.log("Sunrise:", sunrise);
+    console.log("Sunset:", sunset);
 
-        elements.sunrise.textContent = `Sunrise: ${sunrise}`;
-        elements.sunset.textContent = `Sunset: ${sunset}`;
-    }
+    elements.sunrise.textContent = `Sunrise: ${sunrise}`;
+    elements.sunset.textContent = `Sunset: ${sunset}`;
+  }
 
-
-    // Save both weather and astronomy data to localStorage
-    const combinedData = { ...data, astronomy: astronomyData };
-    localStorage.setItem('weatherData', JSON.stringify(combinedData));  
+  // Save both weather and astronomy data to localStorage
+  const combinedData = { ...data, astronomy: astronomyData };
+  localStorage.setItem("weatherData", JSON.stringify(combinedData));
 }
 
 // Function to load weather data
 export function loadWeatherData() {
-    const storedWeatherData = localStorage.getItem('weatherData');
-    if (storedWeatherData) {
-        displayWeather(JSON.parse(storedWeatherData));
-    } else {
-        console.log('No weather data found. Please perform a search.');
-    }
+  const storedWeatherData = localStorage.getItem("weatherData");
+  if (storedWeatherData) {
+    const data = JSON.parse(storedWeatherData);
+    const astronomyData = data.astronomy;
+    delete data.astronomy; // Remove the astronomy property to restore original structure
+    displayWeather(data, astronomyData);
+  } else {
+    console.log("No weather data found. Please perform a search.");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const submitButton = document.getElementById('btnSubmit');
-    const valueCityInput = document.getElementById('valueCity');
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.getElementById("btnSubmit");
+  const valueCityInput = document.getElementById("valueCity");
 
-    // Function to handle weather data fetching
-    const fetchDataAndDisplayWeather = async () => {
-        const apiKey = '6a1dd8448eaa46809c8184729232311'; // Replace with your actual API key
-        const baseUrl = 'https://api.weatherapi.com/v1';
+  // Function to handle weather data fetching
+  const fetchDataAndDisplayWeather = async () => {
+    const apiKey = "6a1dd8448eaa46809c8184729232311";
+    const baseUrl = "https://api.weatherapi.com/v1";
 
-        if (valueCityInput) {
-            let valueCity = valueCityInput.value;
+    if (valueCityInput) {
+      let valueCity = valueCityInput.value;
 
-            if (valueCity === '' || !isNaN(valueCity)) {
-                alert('Insert a city!');
-            } else {
-                const currentApiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${valueCity}`;
-                const astronomyApiUrl = `${baseUrl}/astronomy.json?key=${apiKey}&q=${valueCity}`;
+      if (valueCity === "" || !isNaN(valueCity)) {
+        alert("Insert a city!");
+      } else {
+        const currentApiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${valueCity}`;
+        const astronomyApiUrl = `${baseUrl}/astronomy.json?key=${apiKey}&q=${valueCity}`;
 
-                try {
-                    const [currentData, astronomyData] = await Promise.all([
-                        fetchData(currentApiUrl),
-                        fetchData(astronomyApiUrl)
-                    ]);
-                    displayWeather(currentData);
-                    console.log('Current Data:', currentData);
-                    console.log('Astronomy Data:', astronomyData);
-                } catch (error) {
-                    console.error('Error:', error);
-                    alert('Unknown location, try to enter another');
-                }
-            }
+        try {
+          const [currentData, astronomyData] = await Promise.all([
+            fetchData(currentApiUrl),
+            fetchData(astronomyApiUrl),
+          ]);
+          displayWeather(currentData, astronomyData); // Fixed: passing astronomyData
+          console.log("Current Data:", currentData);
+          console.log("Astronomy Data:", astronomyData);
+        } catch (error) {
+          console.error("Error:", error);
+          alert("Unknown location, try to enter another");
         }
-    };
+      }
+    }
+  };
 
-    // Event listener for the button click
-    submitButton.addEventListener('click', fetchDataAndDisplayWeather);
+  // Event listener for the button click
+  submitButton.addEventListener("click", fetchDataAndDisplayWeather);
 
-    // Event listener for 'Enter' key press in the input field
-    valueCityInput.addEventListener('keydown', async (event) => {
-        if (event.key === 'Enter') {
-            await fetchDataAndDisplayWeather();
-        }
-    });
-    loadWeatherData();
+  // Event listener for 'Enter' key press in the input field
+  valueCityInput.addEventListener("keydown", async (event) => {
+    if (event.key === "Enter") {
+      await fetchDataAndDisplayWeather();
+    }
+  });
+  loadWeatherData();
 });
--- js/dom.js
+++ js/dom.js
@@ -1,128 +1,127 @@
-import { displayWeather, fetchData, loadWeatherData } from './api.js'
+import { displayWeather, fetchData, loadWeatherData } from "./api.js";
 
-const body = document.querySelector('body');
-const weatherApp = document.querySelector('.flex-box-center-column');
-const valueCityInput = document.getElementById('valueCity');
+const body = document.querySelector("body");
+const weatherApp = document.querySelector(".flex-box-center-column");
+const valueCityInput = document.getElementById("valueCity");
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  //Open the page without content, just welcome modal, therefore remove it
+  weatherApp.classList.remove("flex-box-center-column");
+  weatherApp.classList.add("weather-app-disppearing");
 
-    //Open the page without content, just welcome modal, therefore remove it
-    weatherApp.classList.remove('flex-box-center-column');
-    weatherApp.classList.add('weather-app-disppearing');
+  //Create welcome modal and add css class
+  const welcomeModal = document.createElement("div");
+  welcomeModal.classList.add("welcome-modal");
 
-    //Create welcome modal and add css class
-    const welcomeModal = document.createElement('div');
-    welcomeModal.classList.add('welcome-modal');
+  //Create button for open weather app and add css class
+  const welcomeButton = document.createElement("button");
+  welcomeButton.classList.add("welcome-nextbutton");
 
-    //Create button for open weather app and add css class
-    const welcomeButton = document.createElement('button');
-    welcomeButton.classList.add('welcome-nextbutton');
-    
-    //Create title box and and add css class
-    const headingBox = document.createElement('div');
-    headingBox.classList.add('heading-box');
+  //Create title box and and add css class
+  const headingBox = document.createElement("div");
+  headingBox.classList.add("heading-box");
 
-    //Create h1 with content and add css class
-    const welcomeHeading = document.createElement('h1');
-    welcomeHeading.textContent = 'Weathyy';
-    welcomeHeading.classList.add('welcomeModal-heading');
+  //Create h1 with content and add css class
+  const welcomeHeading = document.createElement("h1");
+  welcomeHeading.textContent = "Weathyy";
+  welcomeHeading.classList.add("welcomeModal-heading");
 
-    //Create p element with content and add css class
-    const welcomeUnderHeading = document.createElement('p');
-    welcomeUnderHeading.textContent = 'Weather App';
-    welcomeUnderHeading.classList.add('welcome-underHeading');
+  //Create p element with content and add css class
+  const welcomeUnderHeading = document.createElement("p");
+  welcomeUnderHeading.textContent = "Weather App";
+  welcomeUnderHeading.classList.add("welcome-underHeading");
 
-    //Create img element and append source with css class
-    const welcomeImg = document.createElement('img');
-    welcomeImg.src = 'img/umbrella_start.png';
-    welcomeImg.classList.add('welcome-img');
+  //Create img element and append source with css class
+  const welcomeImg = document.createElement("img");
+  welcomeImg.src = "img/umbrella_start.png";
+  welcomeImg.classList.add("welcome-img");
 
-    //Append two elements into main div with class "headingBox"
-    headingBox.appendChild(welcomeHeading);
-    headingBox.appendChild(welcomeUnderHeading);
+  //Append two elements into main div with class "headingBox"
+  headingBox.appendChild(welcomeHeading);
+  headingBox.appendChild(welcomeUnderHeading);
 
-    //Append all created elements into welcomModal
-    welcomeModal.appendChild(welcomeImg);
-    welcomeModal.appendChild(headingBox);
-    welcomeModal.appendChild(welcomeButton);
+  //Append all created elements into welcomModal
+  welcomeModal.appendChild(welcomeImg);
+  welcomeModal.appendChild(headingBox);
+  welcomeModal.appendChild(welcomeButton);
 
-    //Declare welcomeModal into body selector
-    body.appendChild(welcomeModal);
+  //Declare welcomeModal into body selector
+  body.appendChild(welcomeModal);
 
-    //For clearing modal and displaying weather app
-    function removeWelcomePage() {
-        welcomeModal.classList.add('fade-out', 'hidden');
-        setTimeout(() => {
-            welcomeModal.remove();
-        }, 500
-    )};
+  //For clearing modal and displaying weather app
+  function removeWelcomePage() {
+    welcomeModal.classList.add("fade-out", "hidden");
+    setTimeout(() => {
+      welcomeModal.remove();
+    }, 500);
+  }
 
-    //listener for button in order to display the main content of weather app
-    welcomeButton.addEventListener('click', () => {
-        //Hide a welcome modal and display weather app as well
-        removeWelcomePage();
-        setTimeout(() => {
-            body.appendChild(weatherApp);
-            weatherApp.classList.remove('weather-app-disppearing');
-            weatherApp.classList.add('flex-box-center-column');
-        }, 800);
-        loadWeatherData();
-    });
+  //listener for button in order to display the main content of weather app
+  welcomeButton.addEventListener("click", () => {
+    //Hide a welcome modal and display weather app as well
+    removeWelcomePage();
+    setTimeout(() => {
+      body.appendChild(weatherApp);
+      weatherApp.classList.remove("weather-app-disppearing");
+      weatherApp.classList.add("flex-box-center-column");
+    }, 800);
+    loadWeatherData();
+  });
 });
 
-valueCityInput.addEventListener('keydown', async (event) => {
-    if (event.key === 'Enter') {
-        const apiKey = '6a1dd8448eaa46809c8184729232311'; // Replace with your actual API key
-        const baseUrl = 'https://api.weatherapi.com/v1';
+valueCityInput.addEventListener("keydown", async (event) => {
+  if (event.key === "Enter") {
+    const apiKey = "6a1dd8448eaa46809c8184729232311"; // Replace with your actual API key
+    const baseUrl = "https://api.weatherapi.com/v1";
 
-        if (valueCityInput) {
-            let valueCity = valueCityInput.value;
+    if (valueCityInput) {
+      let valueCity = valueCityInput.value;
 
-            if (valueCity === '' || !isNaN(valueCity)) {
-                alert('Insert a city!');
-            } else {
-                const currentApiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${valueCity}`;
-                const astronomyApiUrl = `${baseUrl}/astronomy.json?key=${apiKey}&q=${valueCity}`;
+      if (valueCity === "" || !isNaN(valueCity)) {
+        alert("Insert a city!");
+      } else {
+        const currentApiUrl = `${baseUrl}/current.json?key=${apiKey}&q=${valueCity}`;
+        const astronomyApiUrl = `${baseUrl}/astronomy.json?key=${apiKey}&q=${valueCity}`;
 
-                try {
-                    const [currentData, astronomyData] = await Promise.all([
-                        fetchData(currentApiUrl),
-                        fetchData(astronomyApiUrl)
-                    ]);
+        try {
+          const [currentData, astronomyData] = await Promise.all([
+            fetchData(currentApiUrl),
+            fetchData(astronomyApiUrl),
+          ]);
 
-                    // Use currentData and astronomyData as needed
-                    displayWeather(currentData, astronomyData);
-                    console.log('Astronomy Data:', astronomyData);
-                    console.log('Current data:', currentData);
+          // Use currentData and astronomyData as needed
+          displayWeather(currentData, astronomyData);
+          console.log("Astronomy Data:", astronomyData);
+          console.log("Current data:", currentData);
 
-                    valueCityInput.value = '';
-                } catch (error) {
-                    console.error('Error: ', error);
-                    alert('Unknown location, try to enter another');
-                }
-            }
+          valueCityInput.value = "";
+        } catch (error) {
+          console.error("Error: ", error);
+          alert("Unknown location, try to enter another");
         }
+      }
     }
+  }
 });
 
-const themeButton = document.querySelector('.theme');
-themeButton.addEventListener('click', () => {
-    toggleTheme();
+const themeButton = document.querySelector(".theme");
+themeButton.addEventListener("click", () => {
+  toggleTheme();
 });
 
 function toggleTheme() {
-    const stylesheet = document.getElementById('stylesheet');
+  const stylesheet = document.getElementById("stylesheet");
 
-    if (stylesheet.getAttribute('href') === 'styles/light.css') {
-        stylesheet.setAttribute('href', 'styles/dark.css');
-        localStorage.setItem('theme', 'dark');
-        themeButton.textContent = 'Light Theme';
-    } else {
-        stylesheet.setAttribute('href', 'styles/light.css');
-        localStorage.setItem('theme', 'light')
-        themeButton.textContent = 'Dark Theme';
-    }
-};
+  if (stylesheet.getAttribute("href") === "styles/light.css") {
+    stylesheet.setAttribute("href", "styles/dark.css");
+    localStorage.setItem("theme", "dark");
+    themeButton.textContent = "Light Theme";
+  } else {
+    stylesheet.setAttribute("href", "styles/light.css");
+    localStorage.setItem("theme", "light");
+    themeButton.textContent = "Dark Theme";
+  }
+}
 
 // document.addEventListener('DOMContentLoaded', () => {
 //     const storedTheme = localStorage.getItem('theme');
